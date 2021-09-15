1bf78ddaf1eec34ca2689cc79a8e2d4578f0b2e6
Алексей Голенков <alexeygolenkov@MacBook-Pro-2.local>
2021-09-15 14:15:34 +0300

    The folder tests was deleted

--- CDserver/tests/custom/auth.py
+++
@@ -1,8 +0,0 @@
-from CDserver import auth
-
-
-class Auth(auth.BaseAuth):
-    def login(self, login, password):
-        if login == "tmp":
-            return login
-        return ""
--- CDserver/tests/custom/rights.py
+++
@@ -1,9 +0,0 @@
-from CDserver import pathutils, rights
-
-
-class Rights(rights.BaseRights):
-    def authorization(self, user, path):
-        sane_path = pathutils.strip_path(path)
-        if sane_path not in ("tmp", "other"):
-            return ""
-        return "RrWw"
