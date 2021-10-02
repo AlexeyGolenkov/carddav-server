3909562ea5fe96da0f4e58f691ec91fa29b0eb94
Алексей Голенков <alexeygolenkov@MacBook-Pro-2.local>
2021-10-02 19:39:41 +0300

    Class rebuilding

--- CDserver/rights/owner_only.py
+++ CDserver/rights/owner_only.py
@@ -4,18 +4,7 @@ class Rights(rights.BaseRights):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._verify_user = self.configuration.get("auth", "type") != "none"
-
-    def authorization(self, user, path):
-        if self._verify_user and not user:
-            return ""
-        sane_path = pathutils.strip_path(path)
-        if "/" not in sane_path:
-            return "RW"
-        if sane_path.count("/") == 1:
-            return "rw"
-        return ""
-
-class Rights(Rights):
+        
     def authorization(self, user, path):
         if self._verify_user and not user:
             return ""
